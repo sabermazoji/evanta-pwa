e614c92b96869438e01276b1e5e3320cf0c1fae5
sabermazoji <saberkurd4@gmail.com>
2019-10-30 00:01:13 +0330

    add navbar cmp

---
+++ src/components/screens/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../elemenets/Navbar';
+import Layout from '../../layouts';
+
+const Home = () => {
+    return (
+        <Layout>
+        <Navbar />
+        </Layout>
+    );
+};
+
+export default Home;
